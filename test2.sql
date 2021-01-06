cb013cda7c2e993d0efebc19020b32a1fe3e4f53
vikas2708 <vikasbca08@gmail.com>
2021-01-06 20:51:09 +0530

    removing all file

--- test2.sql
+++
@@ -1,9 +0,0 @@
-select a, b, c, d from employee
-where name = 'a' 
-group by a
-order by b, c desc 
-select a, b from employee
-where name <> NULL and name = 'a' 
-group by a
-having a is not null
-order by b
